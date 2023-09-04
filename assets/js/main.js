96b234bcf97761bac3cd5b6cfa44ec6e7466381c
moniquetrotta <monique.trotta@gmail.com>
2023-09-04 11:40:30 -0300

    usando map e join para listar pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
   return `
   <li class="pokemon">
@@ -19,14 +15,11 @@ function convertPokemonToLi(pokemon) {
 }
 
 const pokemonList = document.getElementById('pokemonList');
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
 
-      pokemonList.innerHTML += convertPokemonToLi(pokemon);
-    }
-  })
-  .catch((error) => console.error(error));
+pokeApi.getPokemons().then((pokemons = []) => {
+  const newList = pokemons.map((pokemon) => {
+    return convertPokemonToLi(pokemon);
+  });
+  const newHtml = newList.join('');
+  pokemonList.innerHTML += newHtml;
+});
