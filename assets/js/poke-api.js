2518f36c8138a44736008f36e8b229db04fb7791
moniquetrotta <monique.trotta@gmail.com>
2023-09-06 11:45:52 -0300

    chamando detalhes e imagens dos pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {};
-
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
+    .then((response) => response.json()) //converte response para json
+    .then((jsonBody) => jsonBody.results) // pega só os results que é a lista de pokemons
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //transforma lista de pokemons em lista de busca de detalhes
+    .then((detailRequests) => Promise.all(detailRequests)) //esperando que toda lista de requisição termine
+    .then((pokemonsDetails) => pokemonsDetails)
     .catch((error) => console.error(error));
 };
+
+Promise.all([]);
