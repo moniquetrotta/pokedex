c7d47babe1ced0c4286092e26b182d7b01aa5a55
moniquetrotta <monique.trotta@gmail.com>
2023-09-06 11:46:42 -0300

    12 por página

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,7 +2,7 @@ const pokeApi = {};
 pokeApi.getPokemonDetail = (pokemon) => {
   return fetch(pokemon.url).then((response) => response.json());
 };
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 12) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   return fetch(url)
     .then((response) => response.json()) //converte response para json
