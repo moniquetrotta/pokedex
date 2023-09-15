34be6b33956db88e78818a225b16c0efceb4fb68
moniquetrotta <monique.trotta@gmail.com>
2023-09-15 10:02:04 -0300

    maxrecord 151, limit 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMore');
-const maxRecords = 15;
-const limit = 5;
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
